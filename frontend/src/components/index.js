b2efb1b67a1dcc8c49dd06ddaf19911dca0de3bb
Dev-Loigen <loigen.lariosa@student.passerellesnumeriques.org>
2024-06-20 14:03:34 +0800

    [feature][Guest/Buyer]Dev-loigen
    All and Feature page

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,11 +1,15 @@
 import DashboardComponent from "./DashboardComponent";
 import HeaderContainer from "./layout/Navigation/HeaderContainer";
 import SideBar from "./layout/Navigation/SideBar";
-import MainLayout from "./layout/Layout.component";
+import MainLayout from "./layout/layout.component";
+import AllComponent from "./Buyer.AllComponent";
+import FeaturedComponent from "./Buyer.FeaturedComponent";
 
 export {
     DashboardComponent,
     HeaderContainer,
     SideBar,
-    MainLayout
+    MainLayout,
+    AllComponent,
+    FeaturedComponent
 }
--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import MainOutlet from '../pages/MainOutlet';
 import Dashboard from '../pages/Dashboard.page';
+import Allpage from '../pages/Buyer.All.page';
+import Featuredpage from '../pages/Buyer.Featured.page';
 
 const Routes = [
     {
@@ -11,6 +13,14 @@ const Routes = [
             {
                 path: "home",
                 element: <Dashboard/>
+            },
+            {
+                path: "all",
+                element: <Allpage/>
+            },
+            {
+                path: "featured",
+                element: <Featuredpage/>
             }
         ]
     }
