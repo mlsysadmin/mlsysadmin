f138fee62e32e5245f0577ebb8533245035e781a
Dev Estorco <julecenio.estorco@mlhuillier.com>
2024-06-26 08:35:56 +0800

    edited listing form

--- frontend/src/components/seller-broker/ListingForm.jsx
+++ frontend/src/components/seller-broker/ListingForm.jsx
@@ -4,6 +4,7 @@ import Navigation from "../layout/NavigationComponent";
 import { WarningOutlined } from "@ant-design/icons";
 import PropIdLogo from "../../assets/icons/PropertyId/PropIdLogo";
 import floorlogo from "../../assets/images/floorlogo.png";
+import SearchIcon from "../../assets/icons/SearchIcon/SearchIcon";
 
 import ListingSteps from "../layout/ListingSteps";
 import ListingBanner from "../layout/ListingBanner";
@@ -14,7 +15,6 @@ import BathroomInputSlider from "../Slider/BathroomsSlider";
 import NoOfFloorsInputSlider from "../Slider/NoOfFloors";
 
 import "../../styles/listing-form.css";
-
 
 // export default function ColorTabs() {
 //   const [value, setValue] = React.useState('one');
@@ -87,7 +87,9 @@ function ListingDetailsForm() {
                     ].map((tab) => (
                       <div
                         key={tab}
-                        className={`tab ${selectedPropertyTab === tab ? "selected" : ""}`}
+                        className={`tab ${
+                          selectedPropertyTab === tab ? "selected" : ""
+                        }`}
                         onClick={() => handlePropertyTabClick(tab)}
                       >
                         {tab}
@@ -105,7 +107,9 @@ function ListingDetailsForm() {
                       (tab) => (
                         <div
                           key={tab}
-                          className={`tab ${selectedPropertyTab === tab ? "selected" : ""}`}
+                          className={`tab ${
+                            selectedPropertyTab === tab ? "selected" : ""
+                          }`}
                           onClick={() => handlePropertyTabClick(tab)}
                         >
                           {tab}
@@ -123,7 +127,9 @@ function ListingDetailsForm() {
                     {["Warehouse", "Farm Lot", "Hotel/Resort"].map((tab) => (
                       <div
                         key={tab}
-                        className={`tab ${selectedPropertyTab === tab ? "selected" : ""}`}
+                        className={`tab ${
+                          selectedPropertyTab === tab ? "selected" : ""
+                        }`}
                         onClick={() => handlePropertyTabClick(tab)}
                       >
                         {tab}
@@ -147,7 +153,9 @@ function ListingDetailsForm() {
                     {["For Rent", "For Sale", "Pre-Selling"].map((tab) => (
                       <div
                         key={tab}
-                        className={`tab ${selectedListingTab === tab ? "selected" : ""}`}
+                        className={`tab ${
+                          selectedListingTab === tab ? "selected" : ""
+                        }`}
                         onClick={() => handleListingTabClick(tab)}
                       >
                         {tab}
@@ -224,7 +232,9 @@ function ListingDetailsForm() {
                       {["Yes", "No", "Semi"].map((tab) => (
                         <div
                           key={tab}
-                          className={`furnish-tab ${selectedSellingPrice === tab ? "selected" : ""}`}
+                          className={`furnish-tab ${
+                            selectedSellingPrice === tab ? "selected" : ""
+                          }`}
                           onClick={() => handleSellingPriceClick(tab)}
                         >
                           {tab}
@@ -252,7 +262,8 @@ function ListingDetailsForm() {
                     <img
                       className="beds-logo"
                       alt="Beds logo"
-                      src="https://cdn.animaapp.com/projects/64e41d552340cba66b90f01a/releases/665420ac7606a0cc15aa6b94/img/hotel-bed-2--bed-double-bedroom-bedrooms-queen-king-full-hotel-h-5@2x.png" />
+                      src="https://cdn.animaapp.com/projects/64e41d552340cba66b90f01a/releases/665420ac7606a0cc15aa6b94/img/hotel-bed-2--bed-double-bedroom-bedrooms-queen-king-full-hotel-h-5@2x.png"
+                    />
                   </div>
                 </div>
               </div>
@@ -268,13 +279,14 @@ function ListingDetailsForm() {
                 <div className="parking-input-group">
                   <div className="overlap-10">
                     <div className="parking-number">
-                    <ParkingInputSlider />
+                      <ParkingInputSlider />
                     </div>
 
                     <img
                       className="parking-logo"
                       alt="Parking logo"
-                      src="https://cdn.animaapp.com/projects/64e41d552340cba66b90f01a/releases/665e706f7ae3ba3a45818d90/img/parking-sign--discount-coupon-parking-price-prices-hotel.svg" />
+                      src="https://cdn.animaapp.com/projects/64e41d552340cba66b90f01a/releases/665e706f7ae3ba3a45818d90/img/parking-sign--discount-coupon-parking-price-prices-hotel.svg"
+                    />
                   </div>
                 </div>
               </div>
@@ -292,13 +304,15 @@ function ListingDetailsForm() {
                     <img
                       className="floor-logo-img"
                       alt="floor-logo"
-                      src={floorlogo} />
+                      src={floorlogo}
+                    />
                   </div>
 
                   <input
                     id="floorarea-input"
                     className="floorarea-input"
-                    type="text" />
+                    type="text"
+                  />
                   <div className="sqm-prefix">sqm</div>
                 </div>
               </div>
@@ -317,7 +331,8 @@ function ListingDetailsForm() {
                   <input
                     id="pricepersqm"
                     className="pricepersqm-input"
-                    type="text" />
+                    type="text"
+                  />
                 </div>
               </div>
             </div>
@@ -326,7 +341,9 @@ function ListingDetailsForm() {
           {/* right */}
           <div className="unit-details-right">
             <div className="form-group">
-              <div className="text-wrapper-37"> Discounted <br /> Selling Price
+              <div className="text-wrapper-37">
+                {" "}
+                Discounted <br /> Selling Price
               </div>
               <div className="input-group">
                 <label className="text-wrapper-38" htmlFor="disc-selling-price">
@@ -339,7 +356,8 @@ function ListingDetailsForm() {
                   <input
                     id="disc-price-input"
                     className="disc-price-input"
-                    type="text" />
+                    type="text"
+                  />
                 </div>
               </div>
             </div>
@@ -356,7 +374,9 @@ function ListingDetailsForm() {
                       {["BrandNew", "Retail"].map((tab) => (
                         <div
                           key={tab}
-                          className={`classification-tab ${selectedClassification === tab ? "selected" : ""}`}
+                          className={`classification-tab ${
+                            selectedClassification === tab ? "selected" : ""
+                          }`}
                           onClick={() => handleClassificationClick(tab)}
                         >
                           {tab}
@@ -378,13 +398,14 @@ function ListingDetailsForm() {
                 <div className="bathroom-input-group">
                   <div className="overlap-10">
                     <div className="bathroom-number">
-                    <BathroomInputSlider />
+                      <BathroomInputSlider />
                     </div>
-                  
+
                     <img
                       className="bathroom-logo"
                       alt="bathroom-logo"
-                      src="https://cdn.animaapp.com/projects/64e41d552340cba66b90f01a/releases/665e706f7ae3ba3a45818d90/img/hotel-shower-head--bathe-bath-bathroom-shower-water-head-hotel@2x.png" />
+                      src="https://cdn.animaapp.com/projects/64e41d552340cba66b90f01a/releases/665e706f7ae3ba3a45818d90/img/hotel-shower-head--bathe-bath-bathroom-shower-water-head-hotel@2x.png"
+                    />
                   </div>
                 </div>
               </div>
@@ -400,13 +421,14 @@ function ListingDetailsForm() {
                 <div className="nofloors-input-group">
                   <div className="overlap-10">
                     <div className="nofloors-number">
-                    <NoOfFloorsInputSlider />
+                      <NoOfFloorsInputSlider />
                     </div>
-                  
+
                     <img
                       className="nofloors-logo"
                       alt="nofloors-logo"
-                      src="https://cdn.animaapp.com/projects/64e41d552340cba66b90f01a/releases/665e706f7ae3ba3a45818d90/img/descending-number-order-1@2x.png" />
+                      src="https://cdn.animaapp.com/projects/64e41d552340cba66b90f01a/releases/665e706f7ae3ba3a45818d90/img/descending-number-order-1@2x.png"
+                    />
                   </div>
                 </div>
               </div>
@@ -424,18 +446,19 @@ function ListingDetailsForm() {
                     <img
                       className="lot-area-logo-img"
                       alt="lot-area-logo"
-                      src={floorlogo} />
+                      src={floorlogo}
+                    />
                   </div>
 
                   <input
                     id="lot-area-input"
                     className="lot-area-input"
-                    type="text" />
+                    type="text"
+                  />
                   <div className="sqm-prefix">sqm</div>
                 </div>
               </div>
             </div>
-
 
             <div className="form-group">
               <div className="text-wrapper-37">Property ID Number</div>
@@ -452,8 +475,90 @@ function ListingDetailsForm() {
                   <input
                     id="propid-input"
                     className="propid-input"
-                    type="text" />
+                    type="text"
+                  />
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="location-details">
+        <div className="location-label">Location</div>
+        <div className="location-div">
+
+          <div className="form-group">
+            <div className="text-wrapper-37">Subdivision</div>
+            <div className="input-group">
+              <label className="text-wrapper-38" htmlFor="subdivision">
+                What is the subdivision name?
+              </label>
+
+              <div className="subdivision-input-container">
+                <div className="subdivision-logo">
+                  <img
+                    className="subdivision-logo-img"
+                    alt="subdivision-logo"
+                    src="https://cdn.animaapp.com/projects/64e41d552340cba66b90f01a/releases/665e706f7ae3ba3a45818d90/img/location-pin-3--navigation-map-maps-pin-gps-location.svg"
+                  />
+                </div>
+
+                <input
+                  id="subdivision-input"
+                  className="subdivision-input"
+                  type="text"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <div className="text-wrapper-37">Address</div>
+            <div className="input-group">
+              <label className="text-wrapper-38" htmlFor="address">
+                What is the address of the unit?
+              </label>
+
+              <div className="address-input-container">
+                <div className="address-logo">
+                  <img
+                    className="address-logo-img"
+                    alt="address-logo"
+                    src="https://cdn.animaapp.com/projects/64e41d552340cba66b90f01a/releases/665e706f7ae3ba3a45818d90/img/location-pin-3--navigation-map-maps-pin-gps-location.svg"
+                  />
+                </div>
+
+                <input
+                  id="address-input"
+                  className="address-input"
+                  type="text"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <div className="text-wrapper-37">Map Location</div>
+            <div className="input-group">
+              <label className="text-wrapper-38" htmlFor="map-location">
+                Show us where the property located.?
+              </label>
+
+              <div className="map-location-input-container">
+                <div className="map-search-logo">
+                  <SearchIcon className="search"/>
+                </div>
+
+                <input
+                  id="map-location-input"
+                  className="map-location-input"
+                  type="text"
+                />
+              </div>
+
+              <div className="map">
+                
               </div>
             </div>
           </div>
