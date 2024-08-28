a319ffec7100559a41d289f23d1f558847fef690
CHAREBEL BEJOC <BEJO20248201@MLHUILLIER.LOCAL>
2024-08-28 16:49:02 +0800

    postlistings

--- frontend/src/api/PostListings.js
+++ frontend/src/api/PostListings.js
@@ -1,27 +1,112 @@
 import MLBROKERAGEAxiosInstance from "../helper/axios";
 
-const PostSellerListing = async () => {
-  try {
-    const response = await MLBROKERAGEAxiosInstance.post(`api/seller/add-listing`, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        property_type: {
-          type: selectedPropertyTab,
-        },
-        listing_type: selectedListingTab,
-      }),
-    });
 
-    if (response.ok) {
-      console.log("Listing Data Posted");
-    } else {
-      console.error("Error posting data");
-    }
-  } catch (error) {
-    console.log(error);
-  }
+const PostSellerListing = async (property_fields = {}) => {
+	try {
+		const {
+			listing_id,
+			seller,
+			property_id,
+			listing_status,
+			current_level,
+			level,
+			description,
+			property_type: { type, subtype } = {}, // Default to an empty object
+			listing_type_id,
+			unit_details: {
+				price,
+				discounted_price,
+				price_per_sqm,
+				furnishing,
+				classification,
+				no_of_beds,
+				no_of_bathrooms,
+				no_of_floors,
+				parking,
+				floor_area,
+				lot_area,
+			} = {}, // Default to an empty object
+			location: {
+				subdivision,
+				city,
+				province,
+				other,
+				zipcode,
+				map_location,
+			} = {}, // Default to an empty object
+			amenities: {
+				indoor_features,
+				outdoor_features,
+				custom_amenities: { feature_name } = {}, // Default to an empty object
+				custom_inclusion: { inclusion_name } = {}, // Default to an empty object
+			} = {}, // Default to an empty object
+			photos: { photo, upload_date_time } = {}, // Default to an empty object
+		} = property_fields;
+
+		const reqbody = {
+			payload: {
+				listing_id,
+				seller,
+				property_id,
+				listing_status,
+				current_level,
+				level,
+				description,
+				property_type: { type, subtype },
+				listing_type_id,
+				unit_details: {
+					price,
+					discounted_price,
+					price_per_sqm,
+					furnishing,
+					classification,
+					no_of_beds,
+					no_of_bathrooms,
+					no_of_floors,
+					parking,
+					floor_area,
+					lot_area,
+				},
+				location: { subdivision, city, province, other, zipcode, map_location },
+				amenities: {
+					indoor_features,
+					outdoor_features,
+					custom_amenities: { feature_name },
+					custom_inclusion: { inclusion_name },
+				},
+				photos: {
+					photo,
+					upload_date_time,
+				},
+			},
+		};
+
+		console.log("Sending payload:", reqbody);
+
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+				"x-api-key": process.env.REACT_APP_API_KEY,
+			},
+		};
+
+		const response = await MLBROKERAGEAxiosInstance.post(
+			`/api/seller/add-listing`,
+			reqbody,
+			config
+		);
+
+		if (response.status === 200) {
+			console.log("Listing Data Posted Successfully");
+		} else {
+			console.error("Error posting data:", response.statusText);
+		}
+		return response.data;
+	} catch (error) {
+		console.log("Error:", error.message);
+	}
 };
+
+
 
 export default PostSellerListing;
--- frontend/src/components/Slider/NoOfFloors.jsx
+++ frontend/src/components/Slider/NoOfFloors.jsx
@@ -1,34 +1,33 @@
 import React, { useState } from "react";
 
 
-const NoOfFloorsInputSlider = () => {
-  const [bedValue, setBedValue] = useState(0);
+const NoOfFloorsInputSlider = ({value, onChange}) => {
 
   return (
-    <div>
-      <div className="no-of-floors-value">
-        <input
-          id="no-of-floors-input"
-          className="no-of-floors-input"
-          type="text"
-          value={bedValue}
-          readOnly
-        />
-      </div>
+		<div>
+			<div className="no-of-floors-value">
+				<input
+					id="no-of-floors-input"
+					className="no-of-floors-input"
+					type="text"
+					value={value}
+					readOnly
+				/>
+			</div>
 
-      <div className="no-of-floors-slider-container">
-        <input
-          id="no-of-floors-slider"
-          className="no-of-floors-slider"
-          type="range"
-          min="0"
-          max="5"
-          value={bedValue}
-          onChange={(e) => setBedValue(e.target.value)}
-        />
-      </div>
-    </div>
-  );
+			<div className="no-of-floors-slider-container">
+				<input
+					id="no-of-floors-slider"
+					className="no-of-floors-slider"
+					type="range"
+					min="0"
+					max="5"
+					value={value}
+					onChange={(e) => onChange(e.target.value)}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default NoOfFloorsInputSlider;
