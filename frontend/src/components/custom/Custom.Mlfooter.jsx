000798ffb564dc42d1102d0f82588ef96c199be5
CHAREBEL BEJOC <BEJO20248201@MLHUILLIER.LOCAL>
2024-09-13 09:00:00 +0800

    adjustment/frontend-other-services-popUpModal/applicationModal

--- frontend/src/components/custom/Custom.Mlfooter.jsx
+++ frontend/src/components/custom/Custom.Mlfooter.jsx
@@ -10,49 +10,60 @@ import "../../styles/custom.mlfooter.css";
 
 const CustomMlFooter = () => {
     return (
-        <div className="third-content-footer">
-            <div className="third-cont">
-                <Row>
-                    <Col span={8}>
-                        <img src={mllogo} alt="ML Logo" className="logo-ml" />
-                        <div className="customer-care"><CustomerServiceFilled style={{ color: '#ff2800' }} /><span>customercare@mlhuillier.com</span></div>
-                        <div className="firstcol-icons">
-                            <FacebookFilled className="social-icon" />
-                            <TwitterCircleFilled className="social-icon" />
-                            <InstagramFilled className="social-icon" />
-                            <YoutubeOutlined className="social-icon" />
-                        </div>
-                    </Col>
-                    <Col span={5}>
-                        <div className="col-title">Quick Links</div>
-                        <div className="col-cont">
-                            <div>Online Services</div>
-                            <div>Products & Services</div>
-                            <div>About Us</div>
-                            <div>Careers</div>
-                        </div>
-                    </Col>
-                    <Col span={5}>
-                        <div className="col-title">Support</div>
-                        <div className="col-cont">
-                            <div>FAQs</div>
-                            <div>Contact Us</div>
-                            <div>Privacy Notice</div>
-                            <div>Terms and Conditions</div>
-                        </div>
-                    </Col>
-                    <Col span={5}>
-                        <div className="col-title">Downloads</div>
-                        <div className="last-col-icons">
-                            <div> <img src={applestore} alt="Apple App Store" /></div>
-                            <div> <img src={gplaystore} alt="Google Play Store" /></div>
-                            <div><img src={huaweistore} alt="App Gallery" /></div>
-                        </div>
-                    </Col>
-                </Row>
-            </div>
-        </div>
-    );
+			<div className="third-content-footer">
+				<div className="third-cont">
+					<Row>
+						<Col span={8}>
+							<img src={mllogo} alt="ML Logo" className="logo-ml" />
+							<div className="customer-care">
+								<CustomerServiceFilled style={{ color: "#ff2800" }} />
+								<span>customercare@mlhuillier.com</span>
+							</div>
+							<div className="firstcol-icons">
+								<FacebookFilled className="social-icon" />
+								<TwitterCircleFilled className="social-icon" />
+								<InstagramFilled className="social-icon" />
+								<YoutubeOutlined className="social-icon" />
+							</div>
+						</Col>
+						<Col className="col-footer-name" span={5}>
+							<div className="col-title">Quick Links</div>
+							<div className="col-cont">
+								<div>Online Services</div>
+								<div>Products & Services</div>
+								<div>About Us</div>
+								<div>Careers</div>
+							</div>
+						</Col>
+						<Col className="col-footer-name" span={5}>
+							<div className="col-title">Support</div>
+							<div className="col-cont">
+								<div>FAQs</div>
+								<div>Contact Us</div>
+								<div>Privacy Notice</div>
+								<div>Terms and Conditions</div>
+							</div>
+						</Col>
+						<Col span={5} className="col-footer-name">
+							<div className="col-title">Downloads</div>
+							<div className="last-col-icons">
+								<div>
+									{" "}
+									<img src={applestore} alt="Apple App Store" />
+								</div>
+								<div>
+									{" "}
+									<img src={gplaystore} alt="Google Play Store" />
+								</div>
+								<div>
+									<img src={huaweistore} alt="App Gallery" />
+								</div>
+							</div>
+						</Col>
+					</Row>
+				</div>
+			</div>
+		);
 
 
 }
