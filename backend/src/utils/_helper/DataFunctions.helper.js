72d93cf46a4133f76db67501b4b8c3c1f1623df1
Dev-Mobilla <101626428+Dev-Mobilla@users.noreply.github.com>
2024-12-16 13:08:48 +0800

    [Feature][dev-mobilla] modify approved listing image

--- backend/src/utils/_helper/DataFunctions.helper.js
+++ backend/src/utils/_helper/DataFunctions.helper.js
@@ -8,6 +8,32 @@ const StringToArray = (value, separator) => {
     }
 }
 
+const ToPascalCase = (word) => {  
+    try {
+      let capitalizeEachWord = "";
+  
+      if (word === undefined) {
+        capitalizeEachWord = "";
+      } else {
+        let w = ""
+        word.trim().replace(/[\_-]/g, " ").split(' ').forEach((st) => {
+          w += CapitalizeString(st) + " ";
+        })
+  
+        capitalizeEachWord = w
+      }
+      console.log(capitalizeEachWord);
+  
+      capitalizeEachWord.trim();
+  
+      return capitalizeEachWord;
+    } catch (error) {
+      return word
+    }
+  
+  }
+
 module.exports = {
-    StringToArray
+    StringToArray,
+    ToPascalCase
 }
