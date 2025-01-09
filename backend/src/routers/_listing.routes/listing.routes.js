dae42fc6408c9a1088fd97383496a92faed6e2a1
Dev-Mobilla <101626428+Dev-Mobilla@users.noreply.github.com>
2025-01-09 13:50:49 +0800

    [Feature][dev-mobilla] Create crud api for features

--- backend/src/routers/_listing.routes/listing.routes.js
+++ backend/src/routers/_listing.routes/listing.routes.js
@@ -13,17 +13,21 @@ const {
     UploadPhotos,
     GetUploadPhotos
 } = require("../../controllers/_listings/listing.controller");
-const { GetAllAmenities, GetAllFeatures } = require("../../controllers/_listings/feature.controller");
+const { GetAllAmenities, GetAllFeatures, AddFeatures, EditAddedFeatures, DeleteAddedFeatures } = require("../../controllers/_listings/feature.controller");
 const { Validation } = require("../../middleware/_validator/validator.middleware");
 const { 
     AddListing, 
-    listing_id
+    listing_id,
+    AddFeaturesVal
 } = require('../../utils/_helper/validationSchema.helper');
 const verifyApiKey = require("../../middleware/_auth/api.auth.middleware");
 
 // GET
 LISTING_ROUTER.get('/get/all-amenities', GetAllAmenities);
 LISTING_ROUTER.get('/get/all/features', verifyApiKey, GetAllFeatures); // IGOTSOLUTIONS
+LISTING_ROUTER.post('/post/add/features', verifyApiKey, Validation(AddFeaturesVal, "body", "payload"), AddFeatures); // IGOTSOLUTIONS
+LISTING_ROUTER.post('/post/update/features', verifyApiKey, Validation(AddFeaturesVal, "body", "payload"), EditAddedFeatures); // IGOTSOLUTIONS
+LISTING_ROUTER.post('/post/delete/features', verifyApiKey, Validation(AddFeaturesVal, "body", "payload"), DeleteAddedFeatures); // IGOTSOLUTIONS
 LISTING_ROUTER.get('/get/approver', GetApproverByLevel);
 LISTING_ROUTER.get('/get/one-listing', auth, Validation(listing_id, "query", ["listing_id"]), GetListingByMasterId);
 LISTING_ROUTER.post('/upload', upload.any('files'), UploadPhotos);
