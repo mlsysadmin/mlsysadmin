d725482f939bddcd400003aabc78edf9ce2d6c6b
CHAREBEL BEJOC <BEJO20248201@MLHUILLIER.LOCAL>
2025-01-13 17:09:35 +0800

    adjusted developers card

--- frontend/src/components/DeveloperComponent.jsx
+++ frontend/src/components/DeveloperComponent.jsx
@@ -7,7 +7,8 @@ import Pagination from "./custom/pagination/Pagination";
 import developers from "../utils/DevelopersMockData";
 import { GetPhotoWithUrl } from "../utils/GetPhoto";
 import { getDevelopers } from "../api/GetDevelopers";
-import noDevFound from '../asset/no-developer-found.png';
+
+import noDevFound from "../asset/no-developer-found.png";
 import { useNavigate } from "react-router-dom";
 
 const PreSellingComponent = () => {
@@ -18,6 +19,12 @@ const PreSellingComponent = () => {
 	const [developerName, setDeveloperName] = useState("");
 	const [developerLogo, setDeveloperLogo] = useState("");
 	const [saleType, setSaleType] = useState("");
+
+	const TruncateDeveloperName = (text) => {
+		const length = 35;
+		if (!text) return "";
+		return text.length > length ? text.substring(0, length) + "..." : text;
+	};
 
 	const fetchDevelopers = async () => {
 		const developers = await getDevelopers();
@@ -42,9 +49,13 @@ const PreSellingComponent = () => {
 			} else if (timeDifferenceInSeconds < 432000) {
 				updated = `${Math.floor(timeDifferenceInSeconds / 86400)}d`;
 			} else {
-				const dateOptions = { year: "numeric", month: "short", day: "numeric" };
-				updated = updatedAt.toLocaleDateString(undefined, dateOptions);
+				const month = updatedAt.getMonth() + 1;
+				const day = updatedAt.getDate();
+				const year = updatedAt.getFullYear();
+				updated = `${month}-${day}-${year}`;
 			}
+
+			const isUpdatedBackground = timeDifferenceInSeconds < 86400;
 
 			return {
 				id: developer.VendorId,
@@ -52,10 +63,15 @@ const PreSellingComponent = () => {
 				developerName: developer.VendorName,
 				properties: developer.TotalUnits,
 				updated: updated,
+				updatedAt,
+				isUpdatedBackground,
 			};
 		});
+		const sorteddata = developersListings.sort(
+			(a, b) => b.updatedAt - a.updatedAt
+		);
 
-		setDevelopersData(developersListings);
+		setDevelopersData(sorteddata);
 	};
 	useEffect(() => {
 		fetchDevelopers();
@@ -72,30 +88,29 @@ const PreSellingComponent = () => {
 		const params = {
 			d_id: developerId,
 			d_name: developerName,
-			sale_type: saleType
+			sale_type: saleType,
 		};
 
 		const values = Object.keys(params);
 
 		let searchParams = "";
-		const falsy = ["", "null", null, undefined]
+		const falsy = ["", "null", null, undefined];
 
 		values.forEach((p, i) => {
-			
 			if (!falsy.includes(params[p]) && i === 0) {
-				searchParams+= `${p}=${params[p]}`
-			}else if (!falsy.includes(params[p])) {
-				searchParams+= `&${p}=${params[p]}`
+				searchParams += `${p}=${params[p]}`;
+			} else if (!falsy.includes(params[p])) {
+				searchParams += `&${p}=${params[p]}`;
 			}
 		});
-		
+
 		navigate(`/developers/?${searchParams}`);
-	}
+	};
 
 	const handleSearchDeveloper = (e) => {
 		console.log(e);
 		setSaleType(e);
-	}
+	};
 	return (
 		<div className="pre-selling-container">
 			<div className="pre-selling-content">
@@ -116,9 +131,15 @@ const PreSellingComponent = () => {
 							<Select placeholder="Location" allowClear>
 								<Select.Option value="All">All</Select.Option>
 							</Select>
-							<Select placeholder="Listing Type" allowClear onChange={handleSearchDeveloper}>
+							<Select
+								placeholder="Listing Type"
+								allowClear
+								onChange={handleSearchDeveloper}
+							>
 								<Select.Option value="pre-selling">Pre Selling</Select.Option>
-								<Select.Option value="ready for occupancy">Ready for Occupancy</Select.Option>
+								<Select.Option value="ready for occupancy">
+									Ready for Occupancy
+								</Select.Option>
 							</Select>
 						</div>
 					</div>
@@ -126,44 +147,58 @@ const PreSellingComponent = () => {
 						<button>Search</button>
 					</div>
 				</div>
-				{
-					currentCards.length > 0 ?
-						<div className="preselling-developers-card">
-							{
-								currentCards?.map((developer, index) => (
-									<div key={index} className="developers-card"
-									onClick={() => handleDeveloperListing(developer.id, developer.developerName)}
-									>
-										<div className="developers-logo">
-											<img
-												src={developer.logo}
-												alt={`${developer.developerName} Logo`}
-											/>
+				{currentCards.length > 0 ? (
+					<div className="preselling-developers-card">
+						{currentCards?.map((developer, index) => (
+							<div
+								key={index}
+								className="developers-card"
+								onClick={() =>
+									handleDeveloperListing(developer.id, developer.developerName)
+								}
+							>
+								<div className="developers-logo">
+									<img
+										src={developer.logo}
+										alt={`${developer.developerName} Logo`}
+									/>
+								</div>
+								<div className="developers-info">
+									<div className="developers-name">
+										<span>
+											{TruncateDeveloperName(developer.developerName)}
+										</span>
+									</div>
+									<div className="developers-properties-stat">
+										<div className="developers-properties-number">
+											Properties:{developer.properties}
 										</div>
-										<div className="developers-info">
-											<div className="developers-name">
-												<span>{developer.developerName}</span>
-											</div>
-											<div className="developers-properties-stat">
-												<div className="developers-properties-number">
-													{developer.properties} properties
-												</div>
-												<div className="developers-status">{developer.updated}</div>
-											</div>
+										<div
+											className="developers-status"
+											style={{
+												backgroundColor: developer.isUpdatedBackground
+													? "#E3FFF0"
+													: "#f7f7f7",
+												color :developer.isUpdatedBackground ? "#007C14" : "#A4A1A1"
+											}}
+										>
+											Updt {developer.updated}
 										</div>
 									</div>
-								))
-							}
-						</div>
-						: <div className="no-developers-found">
-							<div className="no-dev-found__image-wrapper">
-								<img src={noDevFound} alt="No Developers Found" />
+								</div>
 							</div>
-							<div className="no-dev-found__message">
-								<p>No Real Estate Developers Available</p>
-							</div>
+						))}
+					</div>
+				) : (
+					<div className="no-developers-found">
+						<div className="no-dev-found__image-wrapper">
+							<img src={noDevFound} alt="No Developers Found" />
 						</div>
-				}
+						<div className="no-dev-found__message">
+							<p>No Real Estate Developers Available</p>
+						</div>
+					</div>
+				)}
 
 				{currentCards.length > 0 && (
 					<Pagination
